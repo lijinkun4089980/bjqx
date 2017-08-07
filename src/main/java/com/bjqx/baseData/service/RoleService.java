6947660be427a75baa08662ad86ff7b800e5b77e
lijinkun4089980 <625431565@qq.com>
2017-08-07 14:27:49 +0800

    Initial commit

---
+++ src/main/java/com/bjqx/baseData/service/RoleService.java
@@ -0,0 +1,93 @@
+package com.bjqx.baseData.service;
+
+import com.bjqx.base.common.GlobalConfigure;
+import com.bjqx.baseData.dao.RoleMapper;
+import com.bjqx.baseData.entity.Role;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.annotation.Resource;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Service
+@Transactional(readOnly = true)
+public class RoleService {
+
+    @Resource
+    private RoleMapper roleMapper;
+
+    /**
+     * 分页查询
+     *
+     * @param role
+     * @param pageNo 页码
+     */
+    public PageInfo<Role> queryByPage(Role role, Integer pageNo) {
+        PageHelper.startPage(pageNo, GlobalConfigure.DEFAULT_PAGE_SIZE);
+        Map<String, Object> parameter = new HashMap<String, Object>();
+        // TODO 查询参数
+        List<Role> list = roleMapper.findList(parameter);
+        PageInfo<Role> page = new PageInfo<Role>(list);
+        return page;
+    }
+
+    /**
+     * 新增
+     *
+     * @param role
+     */
+    @Transactional(readOnly = false, rollbackFor = Exception.class)
+    public void addOne(Role role) {
+        if (role == null) {
+            throw new RuntimeException("要保存的对象为空,操作失败.");
+        }
+        this.roleMapper.addOne(role);
+    }
+
+    /**
+     * 修改
+     *
+     * @param role
+     */
+    @Transactional(readOnly = false, rollbackFor = Exception.class)
+    public void updateOne(Role role) {
+        if (role == null) {
+            throw new RuntimeException("要修改的对象为空,操作失败.");
+        }
+        if (role.getId() == null) {
+            throw new RuntimeException("要修改的对象ID为空,操作失败.");
+        }
+        this.roleMapper.updateOne(role);
+    }
+
+    /**
+     * 根据ID删除
+     *
+     * @param id
+     */
+    @Transactional(readOnly = false, rollbackFor = Exception.class)
+    public void delById(Integer id) {
+        if (null == id) {
+            throw new RuntimeException("缺少参数ID,操作失败.");
+        }
+        this.roleMapper.delById(id);
+    }
+
+    /**
+     * 根据ID查询
+     *
+     * @param id
+     * @return Role
+     */
+    public Role findById(Integer id) {
+        if (null == id) {
+            throw new RuntimeException("缺少参数ID,操作失败.");
+        }
+        Role role = this.roleMapper.findById(id);
+        return role;
+    }
+}
---
+++ src/webapp/WEB-INF/view/bootstrap/index2.jsp
@@ -0,0 +1,23 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: 阿坤
+  Date: 2017/8/4
+  Time: 16:06
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ include file="../public/common.jsp" %>
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+<html>
+<head>
+    <title>首页</title>
+    <script type="text/javascript" src="../public/js/jquery-3.2.1.min.js"></script>
+</head>
+<body>
+<h2>初使页面</h2>
+</body>
+<script type="text/javascript">
+    $(function () {
+        alert("666")
+    })
+</script>
+</html>
